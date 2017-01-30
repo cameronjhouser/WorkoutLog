ddd73742a176991271e9ee9ceab68e170e846cde
Cameron Houser <Cameron Houser>
2017-01-30 11:17:49 -0500

    set up server and client- worked

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use('/api/test', function(req, res){
+	res.send("Hello Wolrd");
+});
+
 app.listen(3000,function(){
 	console.log('app is listening on 3000');
 });
