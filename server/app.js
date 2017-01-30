c6ad1d5d4221d8f12e23009a97ee4946b39f4a4d
Cameron Houser <Cameron Houser>
2017-01-30 16:12:06 -0500

    set up data base works

--- server/app.js
+++ server/app.js
@@ -3,46 +3,51 @@ var app = express();
 var bodyParser = require('body-parser');
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('workoutlog', 'postgres', 'Godgives2011',{
+var sequelize = new Sequelize('workoutlog', 'postgres', 'Godgives2011', {
 	host: 'localhost',
 	dialect: 'postgres'
 });
 
 sequelize.authenticate().then(
-	function(){
+	function() {
 		console.log('connected to workoutlog postgres db');
 	},
 	function(err){
-		console.log('err');
+		console.log(err);
 	}
 );
 
+// build a user model in sqllize
 var User = sequelize.define('user', {
-	username:Sequelize.STRING,
-	passwordhash:Sequelize.STRING,
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING,
 });
 
 //creates the table in postgres
 //matches the model we defined
 //Doesn't drop the db
-User.sync();
-// User({ force: true }); //drops the table compeletly (line 27ish)
+User.sync(); 
 
+/********************
+****Danger!!!!: THIS WILL DROP THE WHOLE USER TABLE***	
+User.sync({force: true}); //drops the table compeletly (line 27ish)
+****/
 app.use(bodyParser.json());
-// build a user model in sqllize
 
 app.post('/api/user', function(req, res){
+	//when we post to api user, it will want a user object in the body
 	var username = req.body.user.username;
-	var pass = req.body.user.password;
-	// Need to create a user object and use sequelize to put that user
-	// into our database
+	var pass = req.body.user.password;	//TODO: hash this password - HASH=not human readable
 
-	// Match the model we created above 
+	//Match the model we create above
+	//Sequelize - take the user model and go out to the db and create this:
 	User.create({
 		username: username,
-		passwordhash: ""
-	}).then(
+		passwordhash: ''
+}).then(
+		//Sequelize is going to return the object it created from db.
 		function createSuccess(user){
+			//successful get thsi:
 			res.json({
 				user: user,
 				message: 'create'
@@ -52,22 +57,91 @@ app.post('/api/user', function(req, res){
 			res.send(500, err.message);
 		}
 	);
-
 });
 
-// User.sync({force:true});
-
-// points the express var to the file below
 app.use(require('./middleware/headers'));
 
-// req is linked to api/test - test server connection 
 app.use('/api/test', function(req, res){
-	// app.js client gets this response 
 	res.send("Hello World");
 });
 
-app.listen(3000,function(){
-	console.log('app is listening on 3000');
+app.listen(3000, function(){
+	console.log("app is listening on port 3000");
 });
+
+
+
+// var express = require('express');
+// var app = express();
+// var bodyParser = require('body-parser');
+// var Sequelize = require('sequelize');
+
+// var sequelize = new Sequelize('workoutlog', 'postgres', 'Godgives2011',{
+// 	host: 'localhost',
+// 	dialect: 'postgres'
+// });
+
+// sequelize.authenticate().then(
+// 	function(){
+// 		console.log('connected to workoutlog postgres db');
+// 	},
+// 	function(err){
+// 		console.log('err');
+// 	}
+// );
+
+// // build a user model in sqllize
+// var User = sequelize.define('user', {
+// 	username: Sequelize.STRING,
+// 	passwordhash: Sequelize.STRING,
+// });
+
+// //creates the table in postgres
+// //matches the model we defined
+// //Doesn't drop the db
+// User.sync(); 
+// // User({ force: true }); //drops the table compeletly (line 27ish)
+
+// app.use(bodyParser.json());
+
+// app.post('/api/user', function(req, res){
+// 	//when we post to api user, it will want a user object in the body
+// 	var username = req.body.user.username;
+// 	var pass = req.body.user.password;	//TODO: hash this password - HASH=not human readable
+
+// 	//Match the model we create above
+// 	//Sequelize - take the user model and go out to the db and create this:
+// 	User.create({
+// 		username: username,
+// 		passwordhash: ''
+// }).then(
+// 		//Sequelize is going to return the object it created from db.
+// 		function createSuccess(user){
+// 			//successful get thsi:
+// 			res.json({
+// 				user: user,
+// 				message: 'create'
+// 			});
+// 		},
+// 		function createError(err){
+// 			res.send(500, err.message);
+// 		}
+// 	);
+// });
+
+// // User.sync({force:true});
+
+// // points the express var to the file below
+// app.use(require('./middleware/headers'));
+
+// // req is linked to api/test - test server connection 
+// app.use('/api/test', function(req, res){
+// 	// app.js client gets this response 
+// 	res.send("Hello World");
+// });
+
+// app.listen(3000,function(){
+// 	console.log('app is listening on 3000');
+// });
 
 
