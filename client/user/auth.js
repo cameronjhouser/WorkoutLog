671061a4e915a23e409a0f733921f70c16d58f0d
Cameron Houser <Cameron Houser>
2017-01-31 13:10:53 -0500

    updates

---
+++ client/user/auth.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$.extend(WorkoutLog, {
+		// signup open(
+		signup:function(){
+			// username & password variables
+			var username = $("#su_username").val();
+			var password = $("#su_password").val();
+			// user Object 
+			var user = {
+				user: {
+					username: username, 
+					password: password 
+				}
+			};
+			// signup post 
+			var signup = $.ajax({
+				type: "POST",
+				url: WorkoutLog.API_BASE + "user",
+				data: JSON.stringify( user ),
+				contentType: "application/json"
+			});
+			// signup done/file
+			signup.done(function(data){
+				if(data.sessionToken){
+					WorkoutLog.setAuthHeader(data.sessionToken);
+				}
+
+				$("#signup-modal").modal("hide");
+				$(".disabled").removeClass("disabled");
+				$("#loginout").text("Logout");
+					}).fail(function(){
+				$("#su_error").text("There was an issue with sign up").show();
+			});
+
+		}// MAY BE EXTRA
+		// login method 
+
+		// loginout method 
+	});
+
+	// blind events
+	$("#signup").on("click", WorkoutLog.sighup);
+});
