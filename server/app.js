ebebe12bda3d1bb4020b4714a7c80c0dc6f34ba9
Cameron Houser <Cameron Houser>
2017-01-31 23:47:58 -0500

    almost working

--- server/app.js
+++ server/app.js
@@ -4,16 +4,20 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db.js');
+
 var User = sequelize.import('./models/user');
 
-User.sync(); /*sync({force: true}); Danger: THIS WILL DROP TABLE each time the app starts*/
+// User.sync(); 
+/*sync({force: true}); Danger: THIS WILL DROP TABLE each time the app starts*/
+sequelize.sync();
 app.use(bodyParser.json());
 
-app.use(require('./middleware/headers'));
+app.use(require('./middleware/headers.js'));
 app.use(require('./middleware/validate-sessions'));
-
 app.use('/api/user', require('./routes/user'));
+// login user 
 app.use('/api/login', require('./routes/session'));
+app.use('/api/definition', require('./routes/definition'));
 
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
