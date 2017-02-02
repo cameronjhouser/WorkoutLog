367838feaa5831debedb22b0ce85982cf409b166
Cameron Houser <Cameron Houser>
2017-02-02 15:02:03 -0500

    set up delet button, but history is not showing

--- server/routes/log.js
+++ server/routes/log.js
@@ -46,55 +46,20 @@ router.get('/', function(req, res) {
 		}
 	);
 });
+
+router.delete('/', function(req, res){
+	var data = req.body.log.id;
+	Log 
+		.destroy({
+			where: { id: data }
+		}).then(
+			function deleteLogSuccess(data){
+				res.send("you removed a log");
+			},
+			function deleteLogError(err){
+				res.send(500, err.message);
+			}
+			);
+});
+
 module.exports = router;
-
-// var router = require('express').Router();
-// var sequelize = require('../db');
-// var Log = sequelize.import('../models/log');
-// var User = sequelize.import('../models/user');
-// var Definition = sequelize.import('../models/definition');
-
-// router.post('/', function(req, res){
-// 	// req has some body properties that have a username and parent
-// 	var description = req.body.log.description;
-// 	var results = req.body.log.results;
-// 	var user = req.user;
-// 	var definition = req.body.log.def;
-
-// 	//  Use our sequelize model to create log
-
-// 	Log
-// 		.create({
-// 			description: description,
-// 			results: results,
-// 			owner: user.id,
-// 			def: definition
-// 		})
-// 	.then(
-// 		function createSuccess(log){
-// 			res.json(log);
-// 			},
-// 				function createError(err){
-// 					res.send(500, err.message);
-// 				}
-// 		);
-// });
-
-// router.get('/', function(req, res){
-// 	var userid = req.user.id;
-// 	Log 
-// 	.findAll({
-// 		where: { owner: userid }
-// 	})
-// 	.then(
-// 		function findAllSuccess(data) {
-// 			// console.log(data);
-// 			res.json(data);
-// 		},
-// 		function findAllError(err){
-// 			res.send(500, err.message);
-// 		}
-// 		);
-// });
-
-// module.exports = router;
